1c3bfe30c027e257c1d0db4a7923dcd4ea90f4d7
Evgeniy <zhenya.bigel@mail.ru>
2025-08-04 23:22:08 +0300

    fix: review mistakes

--- autodealer_backend/cars/serializers.py
+++ autodealer_backend/cars/serializers.py
@@ -1,21 +1,17 @@
 from rest_framework import serializers
-
-from .models import Car, Promotion, Supplier
-
+from .models import Car, Supplier, Promotion
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = "__all__"
-
+        fields = '__all__'
 
 class SupplierSerializer(serializers.ModelSerializer):
     class Meta:
         model = Supplier
-        fields = "__all__"
-
+        fields = '__all__'
 
 class PromotionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Promotion
-        fields = "__all__"
+        fields = '__all__'
